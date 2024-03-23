c6768dd88a0a5177993b363e7b802b35a081942f
shunma <shunmakajioka12@gmail.com>
2024-03-24 04:01:17 +0900

    連結リスト

--- javascript/data-structure.js
+++ javascript/data-structure.js
@@ -1,5 +1,6 @@
 //コーディングテスト中のデータ構造
-//paiza ioを実行環境とする
+//paiza ioもしくはデペロッパーツールを実行環境とする
+// gptに聞きながら
 
 
 //ハッシュテーブル　→データの格納
@@ -69,3 +70,86 @@ queue.enqueue(4);
 console.log(queue.dataStore);
 queue.dequeue();
 console.log(queue.dataStore);
+
+//連結リスト
+//→データ要素をノードと呼ばれる　オブジェクトで連結したデータ構造。挿入や削除などの操作がリストの先頭や任意の位置で効率的に行える。要素を直接アクセスするためには、リストの先頭から順番に要素をたどる必要がある
+
+//連結リストのノードを表すコンストラクタ関数
+function ListNode(val,next){
+    this.val = val === undefined ? 0 : val; //値
+    this.next = next === undefined ? null : next; //次のノードへの参照
+}
+
+//配列から連結リストを構築する関数
+const makeList = (protoArr) => {
+    let list = new ListNode(protoArr[0]);
+    let pre = list;
+    for(let i = 1; i < protoArr.length; i++){
+        pre.next = new ListNode(protoArr[i]);
+        pre = pre.next;
+    }
+    return list;
+}
+
+//連結リストから特定の値を取り除く関数
+const removeValue = (head,value) => {
+    //先頭のノードが目的の値と一致する場合
+    while(head && head.val === value){
+        head = head.next
+    } 
+    //先頭以外のノードを精査
+    let current = head;
+    while(current && current.next){
+        if(current.next.val === value){
+        // 目的の値を持つ次のノードをスキップ
+        current.next = current.next.next;
+        } else{
+            //目的の値ではない場合、次のノードに進む
+            current = current.next;     
+        }
+        return head;
+    }
+}
+
+//新しい値を追加する
+const addValue = (head,value) => {
+    const newNode = new ListNode(value); //新しいノードを作成
+
+    if(!head){
+        return newNode; //リストが空の場合、新しいノードを先頭に設定
+    }
+
+    let current = head;
+    while(current.next){
+        current = current.next //リストの末尾まで移動
+    }
+
+    current.next = newNode;  //新しいノードをリストに追加
+    return head;
+}
+
+
+//連結リストの表示
+const printList = (head) => {
+    let current = head;
+    while(current){
+        console.log(current.val);
+        current = current.next;
+    }
+}
+
+
+//使用例　配列から連結リストを構築
+const List = makeList([1,2,3,4,5,6]);
+console.log(List); //1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+printList(List);
+removeValue(List , 2);
+console.log(List);
+printList(List);
+addValue(List, 55);
+console.log(List);
+printList(List);
+
+
+
+//木探索
